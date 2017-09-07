5e317180f1ce67500360805c6dfcee8f8fab5bf5
刘福云 <920507252@qq.com>
2017-09-07 14:29:07 +0800

    dict-set

---
+++ 03-dict-set/index0.py
@@ -0,0 +1,22 @@
+"""Build an index mapping word -> list of occurrences"""
+
+import sys
+import re
+
+WORD_RE = re.compile('\w+')
+
+index = {}
+with open(sys.argv[1], 'rt', encoding='utf-8') as f:
+    for line_no, line in enumerate(f, 1):
+        for match in WORD_RE.finditer(line):
+            word = match.group()
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            # this is ugly,coded like this to make point
+            occurrences = index.get(word, [])
+            occurrences.append(location)
+            index[word] = occurrences
+
+
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
